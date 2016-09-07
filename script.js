302ab910c18c2c44f90bf3662f1610c105b3c865
Maia Stone <maiadov@gmail.com>
2016-09-07 15:59:46 -0600

    add remove button functionality

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ var $titleInput = $('#title');
 var $bodyInput = $('#body');
 
 function addNewCard() {
-$('.card').append(`<header>
+$('.card').prepend(`<header>
   <h3 class="card-title">${$titleInput.val()}</h3>
-  <button class="card-delete"></button>
+  <button class="card-delete">delete</button>
 </header>
 <p class="card-body">${$bodyInput.val()}</p>
 <footer>
-  <button class="up-vote"></button>
-  <button class="down-vote"></button>
+  <button class="up-vote">up</button>
+  <button class="down-vote">down</button>
   <p class="quality">Quality</p>
 </footer>`);
 }
 
-
 $('#save').on('click', function() {
   addNewCard();
+});
+
+
+$('.card').on('click', '.card-delete', function() {
+  $('.card').remove();
 });
