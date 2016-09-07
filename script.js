6042691f71b0b26c81b8dd05a162ba7aa2800b1e
Maia Stone <maiadov@gmail.com>
2016-09-07 15:28:14 -0600

    user input creates card

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+var $titleInput = $('#title');
+var $bodyInput = $('#body');
 
 function addNewCard() {
-  var $titleInput = $('#title').val();
-  var $bodyInput = $('#body').val();
-  $('.card-title').prepend($titleInput);
-  $('.card-body').append($bodyInput);
+$('.card').append(`<header>
+  <h3 class="card-title">${$titleInput.val()}</h3>
+  <button class="card-delete"></button>
+</header>
+<p class="card-body">${$bodyInput.val()}</p>
+<footer>
+  <button class="up-vote"></button>
+  <button class="down-vote"></button>
+  <p class="quality">Quality</p>
+</footer>`);
 }
 
 
