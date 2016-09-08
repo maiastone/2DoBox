4084b529a4b83c5a7f867926f72617c75db790e9
Maia Stone <maiadov@gmail.com>
2016-09-07 18:13:50 -0600

    Object constructor generates new idea

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
-var $titleInput = $('#title');
-var $bodyInput = $('#body');
 
-function addNewCard() {
-$('.card').prepend(`<header>
-  <h3 class="card-title">${$titleInput.val()}</h3>
-  <button class="card-delete">delete</button>
-</header>
-<p class="card-body">${$bodyInput.val()}</p>
-<footer>
-  <button class="up-vote">up</button>
-  <button class="down-vote">down</button>
-  <p class="quality">Quality</p>
-</footer>`);
+
+function addNewCard(title, body, id) {
+  $('.card').prepend(`<section id=${id}><header><h3 class="card-title">${title}</h3>
+    <button class="card-delete">delete</button>
+  </header>
+  <p class="card-body">${body}</p>
+  <footer>
+    <button class="up-vote">up</button>
+    <button class="down-vote">down</button>
+    <p class="quality">Quality</p>
+  </footer></section>`);
 }
 
+
 $('#save').on('click', function() {
-  addNewCard();
+  var $titleInput = $('#title').val();
+  var $bodyInput = $('#body').val();
+
+  buildAndRenderIdea($titleInput, $bodyInput);
 });
 
+function Idea (title, body) {
+    this.id = Date.now();
+    this.title = title;
+    this.body = body;
+    this.quality = 'swill';
+}
 
-$('.card').on('click', '.card-delete', function() {
-  $('.card').remove();
-});
+function buildAndRenderIdea(title, body) {
+  var idea = new Idea(title, body);
+  addNewCard(idea.title, idea.body, idea.id);
+  var stringIdea = JSON.stringify(idea);
+  localStorage.setItem('ideas', stringIdea);
+}
+
+
+
+// stringify this new idea and set it to localStorage
+
+// store new ideas into array available everywhere and set into localStorage
+// push each new item into array
+
+// $('.card').on('click', '.card-delete', function() {
+//   $('.card').remove();
+// });
